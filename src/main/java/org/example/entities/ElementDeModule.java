a5a4db9552cc3f8d8aeddbf87da20dba6e7fcf60
hamzanachid <hamzanachid82@gmail.com>
2024-12-29 18:16:25 +0100

    Utilisateur feat , entities

---
+++ src/main/java/org/example/entities/ElementDeModule.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class ElementDeModule {
+    private int id;
+    private Module module;
+    private String nom;
+    private double coefficient;
+    private List<ModaliteEvaluation> modalitesEvaluation;
+
+    // Getters and Setters
+    // Constructors
+}
