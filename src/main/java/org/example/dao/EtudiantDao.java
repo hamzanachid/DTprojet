ecfb5547209f7215572ac97b3b975bb167e3bd82
grainme <marouanebouf3@gmail.com>
2025-01-05 12:22:16 +0100

    Mockito Tests done for "Etudiant" Service class.

--- src/main/java/org/example/dao/EtudiantDao.java
+++ src/main/java/org/example/dao/EtudiantDao.java
@@ -1,8 +1,6 @@
 package org.example.dao;
 
 import org.example.entities.Etudiant;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public interface EtudiantDao {
@@ -10,7 +8,7 @@ public interface EtudiantDao {
     boolean update(Etudiant etudiant, Etudiant newEtudiant);
     boolean delete(Long id);
     Etudiant findById(Long id);
-    public List<Etudiant> findEtudiantByElement(Long element_id);
+    List<Etudiant> findEtudiantByElement(Long element_id);
     List<Etudiant> findByFiliere(Long filiereId);
     List<Etudiant> findAll();
     Etudiant getByMatricule(String matricule);
