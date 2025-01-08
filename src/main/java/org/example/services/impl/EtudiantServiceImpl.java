a326d4e0b4aad1ef7adfbe5a853bdcaff8ab5800
grainme <marouanebouf3@gmail.com>
2025-01-08 19:33:39 +0100

    Mockito Tests done for "Etudiant" Service class.

--- src/main/java/org/example/services/impl/EtudiantServiceImpl.java
+++ src/main/java/org/example/services/impl/EtudiantServiceImpl.java
@@ -4,6 +4,10 @@ package org.example.services.impl;
 import org.example.dao.EtudiantDao;
 import org.example.entities.Etudiant;
 import org.example.services.EtudiantService;
+import org.example.validation.BasicValidationStrategy;
+import org.example.validation.CompositeValidationStrategy;
+import org.example.validation.ValidationStrategy;
+
 import java.util.List;
 
 import static org.example.utils.CheckAccess.checkUserAccess;
@@ -11,22 +15,28 @@ import static org.example.utils.CheckAccess.checkUserAccess;
 public class EtudiantServiceImpl implements EtudiantService {
     public static EtudiantService instance;
     private final EtudiantDao etudiantDao;
+    private final ValidationStrategy validationStrategy;
+
 
     private EtudiantServiceImpl(EtudiantDao etudiantDao) {
         this.etudiantDao = etudiantDao;
+        this.validationStrategy = new CompositeValidationStrategy(new BasicValidationStrategy(), new BasicValidationStrategy());
     }
+
     public static EtudiantService getInstance(EtudiantDao etudiantDao) {
         if(instance == null) {
             instance = new EtudiantServiceImpl(etudiantDao);
         }
         return instance;
     }
+
      @Override
     public Etudiant create(Etudiant etudiant) {
+        validationStrategy.validate(etudiant);
         checkUserAccess();
+        return etudiantDao.create(etudiant);
+    }
 
-         return etudiantDao.create(etudiant);
-     }
     @Override
     public boolean delete(Long id) {
         return etudiantDao.delete(id);
@@ -39,13 +49,10 @@ public class EtudiantServiceImpl implements EtudiantService {
 
     @Override
     public void update(Etudiant etudiant, Etudiant newEtudiant) {
-
+        validationStrategy.validate(etudiant);
         checkUserAccess();
-
         etudiantDao.update(etudiant, newEtudiant);
     }
-
-
 
     @Override
     public Etudiant getById(Long id) {
--- src/main/java/org/example/validation/MatriculeValidationStrategy.java
+++ src/main/java/org/example/validation/MatriculeValidationStrategy.java
@@ -6,8 +6,8 @@ public class MatriculeValidationStrategy implements ValidationStrategy {
     @Override
     public void validate(Etudiant student) {
         String matricule = student.getMatricule();
-        if (!matricule.matches("^[0-9]{8}$")) {
-            throw new IllegalArgumentException("matricule must be 8 digits");
+        if (!matricule.matches("^[0-9]{3}$")) {
+            throw new IllegalArgumentException("matricule must be 3 digits");
         }
     }
 }
